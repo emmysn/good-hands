c419910df0e4c10418f462074313113af06942f5
emmysn <158768897+emmysn@users.noreply.github.com>
2024-02-04 06:46:29 +0100

    Add files via upload

---
+++ project 2 game website/index.js
@@ -0,0 +1,124 @@
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.hero2', slideUp);
+
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.reveal1', slideUp);
+
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.hero6', slideUp);
+
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.hero13', slideUp);
+
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.hero0', slideUp);
+
+var slideUp = {
+    distance: '150%',
+    origin: 'left',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('box7', slideUp);
+
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('mind1', slideUp);
+
+var slideSide = {
+    distance: '150%',
+    origin: 'left',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.box1', slideSide);
+
+var slideSide = {
+    distance: '150%',
+    origin: 'right',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.box2', slideSide);
+
+var slideSide = {
+    distance: '150%',
+    origin: 'left',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.box3', slideSide);
+
+
+
+var slideDown = {
+    distance: '150%',
+    origin: 'top',
+    opacity: null,
+    duration: 3000,
+    reset:  true
+};
+
+ScrollReveal().reveal('.hero11', slideDown);
+
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 3000,
+    reset: true
+};
+
+ScrollReveal().reveal('.hero8', slideUp);
+
+
+ScrollReveal().reveal('.hero14', {duration: 2000, reset: true});
